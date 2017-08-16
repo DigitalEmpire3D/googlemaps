70c5335d45c6fd3f14d843e558d594c6bfde6c3e
DigitalEmpire3D <hhartford@digitalempire3d.com>
2017-08-16 10:24:26 -0600

    updates to reset the map
    added in a couple more ways to reset the map

--- googlemapErie/map-javascript.js
+++ googlemapErie/map-javascript.js
@@ -6,6 +6,7 @@ $(function() {
 			$('input.amenities-input').prop('checked', false).change();
 		}
 	});
+	
 
 	$('a#clear-directions').on('click', function(event) {
 		event.preventDefault();
@@ -19,6 +20,15 @@ $(function() {
 	}
 
 	$('select[name="select-list"]').on('change', function() {
+		map.setCenter(pyrmont),
+		map.setZoom(14),
+		directionsDisplay.setMap(null);
+		directionsDisplay.setPanel(null);
+		service = new google.maps.places.PlacesService(map);
+		directionsDisplay = new google.maps.DirectionsRenderer;
+		directionsDisplay.setMap(map);
+		directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+		directionsService = new google.maps.DirectionsService;
 		//console.log($(this).val());
 		//movie_theater_markers[i].setMap(null);
 		for(var i = 0; i < static_markers.length; i++) {
@@ -52,6 +62,15 @@ $(function() {
 
 	$('.amenities-input').on('change', function() {
 		var type_search = $(this).val();
+		map.setCenter(pyrmont),
+		map.setZoom(14),
+		directionsDisplay.setMap(null);
+		directionsDisplay.setPanel(null);
+		service = new google.maps.places.PlacesService(map);
+		directionsDisplay = new google.maps.DirectionsRenderer;
+		directionsDisplay.setMap(map);
+		directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+		directionsService = new google.maps.DirectionsService;
 		if($(this).is(':checked')) {
 
 			if (type_search == "school") {
